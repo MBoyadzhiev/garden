0ba42d6dae185b087c09ac2388aa399f915e96a6
clouddo-team <dev@clouddo.eu>
2025-10-07 18:56:30 +0300

    changes made 2

--- components/ui/sketch-button.tsx
+++ components/ui/sketch-button.tsx
@@ -5,6 +5,7 @@ interface SketchButtonProps {
   onClick?: () => void;
   className?: string;
   type?: "button" | "submit" | "reset";
+  style?: React.CSSProperties;
 }
 
 export const SketchButton = ({
@@ -12,12 +13,14 @@ export const SketchButton = ({
   onClick,
   className = "",
   type = "button",
+  style,
 }: SketchButtonProps) => {
   return (
     <button
       type={type}
       onClick={onClick}
       className={`rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200 px-4 py-2 ${className}`}
+      style={style}
     >
       {children}
     </button>
